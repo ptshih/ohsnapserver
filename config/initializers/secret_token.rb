0f99d03788308bc535ba616518a85e10518013a7
Peter Shih <ptshih@gmail.com>
2011-01-31 19:28:20 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moogle::Application.config.secret_token = '49463cba2dae3e2aaf0e71a4086361c0bb6bdb9d01445c226312679beb18c1528debb036e15de81853f868f956e6cf0d84e299c3421e9c12b5a3a2b570247ce7'
