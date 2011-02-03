2b752a4dfbc0505e3f0a2389343318df350024dc
Peter Shih <ptshih@gmail.com>
2011-02-02 18:48:35 -0800

    stubs

--- lib/facebook.rb
+++ lib/facebook.rb
@@ -2,6 +2,9 @@ module API
   class Facebook < Api
     
     @@peter_id = 548430564
+    # Peter's access token
+    # https://graph.facebook.com/548430564/checkins?access_token=H_U8HT7bMvsDjEjb8oOjq4qWaY-S7MP8F5YQFNFzggQ.eyJpdiI6Ino1LXpBQ0pNRjJkNzM3YTdGRDhudXcifQ.h5zY_4HM_Ir3jg4mnyySYRvL26DxPgzg3NSI4Tcn_1bXn1Fqdgui1X7W6pDmJQagM5fXqCo7ie4EnCsi2t8OaMGVSTAZ-LSn9fuJFL-ucYj3Siz3bW17Dn6kMDcwxA3fghX9tUgzK0Vtnli6Sn1afA
+    
     def self.find_checkins_for_facebook_id(facebook_id)
     end
   end
