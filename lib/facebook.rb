fc9762321df502709383a12e68c9448200ca43d4
Peter Shih <ptshih@gmail.com>
2011-02-02 21:28:30 -0800

    adding more facebook apis

--- lib/facebook.rb
+++ lib/facebook.rb
@@ -2,21 +2,67 @@ module API
   class Facebook < Api
     @@fbHost = 'https://graph.facebook.com'
     @@peterId = 548430564
+    
+    @@peterLatitude = 37.765223405331
+    @@peterLongitude = -122.45003812016
+    
     # Peter's access token
     @@peterAccessToken = "H_U8HT7bMvsDjEjb8oOjq4qWaY-S7MP8F5YQFNFzggQ.eyJpdiI6Ino1LXpBQ0pNRjJkNzM3YTdGRDhudXcifQ.h5zY_4HM_Ir3jg4mnyySYRvL26DxPgzg3NSI4Tcn_1bXn1Fqdgui1X7W6pDmJQagM5fXqCo7ie4EnCsi2t8OaMGVSTAZ-LSn9fuJFL-ucYj3Siz3bW17Dn6kMDcwxA3fghX9tUgzK0Vtnli6Sn1afA"
     # https://graph.facebook.com/548430564/checkins?access_token=H_U8HT7bMvsDjEjb8oOjq4qWaY-S7MP8F5YQFNFzggQ.eyJpdiI6Ino1LXpBQ0pNRjJkNzM3YTdGRDhudXcifQ.h5zY_4HM_Ir3jg4mnyySYRvL26DxPgzg3NSI4Tcn_1bXn1Fqdgui1X7W6pDmJQagM5fXqCo7ie4EnCsi2t8OaMGVSTAZ-LSn9fuJFL-ucYj3Siz3bW17Dn6kMDcwxA3fghX9tUgzK0Vtnli6Sn1afA
     
     def self.find_checkins_for_facebook_id(facebookId = @@peterId)
-      shouldAcceptCompressedResponse = false
       headersHash = Hash.new
-      headersHash['Accept'] = "application/json"
-      if shouldAcceptCompressedResponse then
-        headersHash['Accept-Encoding'] = "gzip"
+      headersHash['Accept'] = 'application/json'
+      headersHash['Accept-Encoding'] = 'gzip'
+      
+      paramsHash = Hash.new
+      paramsHash['access_token'] = @@peterAccessToken
+      
+      response = Typhoeus::Request.get("#{@@fbHost}/#{facebookId}/checkins", :params => paramsHash, :headers => headersHash, :disable_ssl_peer_verification => true)
+      p response.headers_hash
+      p response.body
+    end
+    
+    # https://graph.facebook.com/search?type=checkin&access_token=ACCESS_TOKEN
+    def self.find_all_checkins
+      headersHash = Hash.new
+      headersHash['Accept'] = 'application/json'
+      headersHash['Accept-Encoding'] = 'gzip'
+      
+      paramsHash = Hash.new
+      paramsHash['access_token'] = @@peterAccessToken
+      paramsHash['type'] = 'checkin'
+      
+      response = Typhoeus::Request.get("#{@@fbHost}/search", :params => paramsHash, :headers => headersHash, :disable_ssl_peer_verification => true)
+      p response.headers_hash
+      p response.body
+    end
+    
+    # https://graph.facebook.com/search?q=pizza&type=place&center=lat,long&distance=1000
+    def self.find_places_near_location(lat = nil, lng = nil, distance = 1000, query = nil)
+      # query is optional
+      
+      # debug
+      lat = @@peterLatitude
+      lng = @@peterLongitude
+      
+      headersHash = Hash.new
+      headersHash['Accept'] = 'application/json'
+      headersHash['Accept-Encoding'] = 'gzip'
+  
+      paramsHash = Hash.new
+      paramsHash['access_token'] = @@peterAccessToken
+      paramsHash['type'] = 'place'
+      paramsHash['center'] = "#{lat},#{lng}"
+      paramsHash['distance'] = "#{distance}"
+      if not query.nil?
+        paramsHash['q'] = "#{query}"
       end
       
-      
-      response = Typhoeus::Request.get("#{@@fbHost}/#{facebookId}/checkins", :params => {:access_token => @@peterAccessToken}, :headers => headersHash, :disable_ssl_peer_verification => true)      
+      response = Typhoeus::Request.get("#{@@fbHost}/search", :params => paramsHash, :headers => headersHash, :disable_ssl_peer_verification => true)
+      p response.headers_hash
       p response.body
+      
     end
   end
 end
