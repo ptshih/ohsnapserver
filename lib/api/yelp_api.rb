678e7dddc4ed7abce9fd0a03518379f0c0794d24
Tom Liou <tliou@raybeam.com>
2011-02-04 03:05:57 -0800

    Yelp serializer

--- lib/api/yelp_api.rb
+++ lib/api/yelp_api.rb
@@ -11,6 +11,34 @@ module API
     @@token = 'ifKMaMyp7X9JqmCePD3BzskBGYZ1q0Tb'
     @@tokenSecret = 'vKrHGl5-gGin81a6Mb5ZIwjiHd0'
     
+    
+    # Create or update yelp in model/database
+    def self.serialize_yelp(yelp)
+       y = Yelp.find_or_initialize_by_place_id(yelp['id'])
+       y.place_id = yelp['id']
+       y.name = yelp['name']
+       y.phone = yelp['phone']
+       y.review_count = yelp['review_count']
+       y.lat = yelp['coordinate']['latitude']
+       y.lng = yelp['coordinate']['longitude']
+       y.raw_hash = yelp
+       y.expires_at = Time.now + 1.days
+       y.save
+       
+       r = YelpReview.find_or_initialize_by_yelp_review_id(yelp['reviews']['id'])
+       r.yelp_id = yelp['id']
+       r.excerpt = yelp['reviews']['excerpt']
+       r.rating = yelp['reviews']['rating']
+       r.time_created = Time.parse(yelp['review']['time_created'])
+       r.user_name = yelp['reviews']['user']['name']
+       r.user_id = yelp['reviews']['user']['id']
+       r.raw_hash = yelp['reviews']
+       r.save
+       
+    end
+    
+    
+    
     def self.find_business_by_id(id = nil)
       id = "yelp-san-francisco"
       path = "/v2/business/#{id}"
