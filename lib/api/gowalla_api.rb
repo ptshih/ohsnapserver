3356d34035c483053e4d8cf291deb63302b6ee25
Tom Liou <tliou@raybeam.com>
2011-02-04 18:16:10 -0800

    Gowalla API stubs

---
+++ lib/api/gowalla_api.rb
@@ -0,0 +1,24 @@
+module API
+  class GowallaApi < Api
+    
+    # User: Moogle
+    # Password: rmoogle99
+    # curl -H 'X-Gowalla-API-Key: blah' -H 'Accept: application/json' -H 'Content-Type: application/json' -u Moogle:rmoogle99 http://api.gowalla.com/spots?lat=30.2697&lng=-97.7494&radius=50
+
+    def self.serialize_gowalla(gowalla)
+
+       
+    end
+    
+    
+    
+    def self.find_business_by_id(id = nil)
+
+    end
+    
+    def self.find_business_by_location(term, latitude, longitude, accuracy, altitude, altitudeAccuracy
+      
+    end
+    
+  end
+end
