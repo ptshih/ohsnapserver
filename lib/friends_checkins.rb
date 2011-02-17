d313abdc8ba8288cf7e11fd053df7ffeccdefbcb
Peter Shih <ptshih@gmail.com>
2011-02-16 22:08:55 -0800

    switching to use the hydra batch http request model because it is 1. faster 2. avoids errors when multiquery exceeds 100+ ids

--- lib/friends_checkins.rb
+++ lib/friends_checkins.rb
@@ -2,6 +2,6 @@ class FriendsCheckins < Struct.new(:facebook_api, :facebook_id, :facebook_id_arr
   
   def perform
     # Find checkins for current user and friends of the current user
-    facebook_api.find_checkins_for_facebook_id_array(facebook_id, facebook_id_array, since)
+    facebook_api.find_checkins_for_facebook_id_array_batch(facebook_id, facebook_id_array, since)
   end
 end
