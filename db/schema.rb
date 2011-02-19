89552fab63efa8148e3c36d39f38b1dbfa83e9f4
Peter Shih <ptshih@gmail.com>
2011-02-18 23:28:15 -0800

    db schema dump

--- db/schema.rb
+++ db/schema.rb
@@ -19,21 +19,46 @@ ActiveRecord::Schema.define(:version => 20110202082319) do
     t.datetime "updated_at"
   end
 
+  add_index "apps", ["app_id"], :name => "idx_app_id", :unique => true
+
   create_table "checkins", :force => true do |t|
-    t.integer  "checkin_id",      :limit => 8, :default => 0
-    t.integer  "facebook_id",     :limit => 8, :default => 0
-    t.integer  "place_id",        :limit => 8, :default => 0
-    t.integer  "app_id",  :limit => 8, :default => 0
+    t.integer  "checkin_id",   :limit => 8, :default => 0
+    t.integer  "facebook_id",  :limit => 8, :default => 0
+    t.integer  "place_id",     :limit => 8, :default => 0
+    t.integer  "app_id",       :limit => 8, :default => 0
     t.string   "message"
     t.datetime "created_time"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "tagged_users", :force => true do |t|
-    t.integer  "checkin_id",  :limit => 8, :default => 0
-    t.integer  "facebook_id", :limit => 8, :default => 0
-    t.string   "name"
+  add_index "checkins", ["checkin_id"], :name => "idx_checkin_id", :unique => true
+  add_index "checkins", ["facebook_id"], :name => "idx_facebook_id"
+
+  create_table "checkins_users", :force => true do |t|
+    t.integer "checkin_id",  :limit => 8, :default => 0
+    t.integer "facebook_id", :limit => 8, :default => 0
+  end
+
+  add_index "checkins_users", ["checkin_id"], :name => "idx_checkin_id"
+
+  create_table "delayed_jobs", :force => true do |t|
+    t.integer  "priority",   :default => 0
+    t.integer  "attempts",   :default => 0
+    t.text     "handler"
+    t.text     "last_error"
+    t.datetime "run_at"
+    t.datetime "locked_at"
+    t.datetime "failed_at"
+    t.text     "locked_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "friends", :force => true do |t|
+    t.integer "facebook_id", :limit => 8, :default => 0
+    t.integer "friend_id",   :limit => 8, :default => 0
+    t.integer "degree",                   :default => 0
   end
 
   create_table "gowallas", :force => true do |t|
@@ -41,10 +66,10 @@ ActiveRecord::Schema.define(:version => 20110202082319) do
     t.integer  "place_id",       :limit => 8,                                 :default => 0
     t.string   "name"
     t.integer  "checkins_count"
-    t.decimal  "lat",                 :precision => 20, :scale => 16
-    t.decimal  "lng",                 :precision => 20, :scale => 16
-    t.string   "raw_hash" # this stores the parsed json, raw hash from gowalla, in case we need to parse stuff out of it later
-    t.datetime "expires_at" # this tells the scraper when it should rescrape the place
+    t.decimal  "lat",                         :precision => 20, :scale => 16
+    t.decimal  "lng",                         :precision => 20, :scale => 16
+    t.string   "raw_hash"
+    t.datetime "expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -54,21 +79,30 @@ ActiveRecord::Schema.define(:version => 20110202082319) do
     t.integer  "yelp_id",    :limit => 8,                                 :default => 0
     t.integer  "gowalla_id", :limit => 8,                                 :default => 0
     t.string   "name"
-    t.decimal  "lat",               :precision => 20, :scale => 16
-    t.decimal  "lng",               :precision => 20, :scale => 16
+    t.decimal  "lat",                     :precision => 20, :scale => 16
+    t.decimal  "lng",                     :precision => 20, :scale => 16
     t.string   "street"
     t.string   "city"
     t.string   "state"
     t.string   "country"
     t.string   "zip"
-    t.string   "raw_hash" # this stores the parsed json, raw hash from facebook, in case we need to parse stuff out of it later
-    t.datetime "expires_at" # this tells the scraper when it should rescrape the place
+    t.string   "raw_hash"
+    t.datetime "expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
+  create_table "tagged_users", :force => true do |t|
+    t.integer "checkin_id",  :limit => 8, :default => 0
+    t.integer "facebook_id", :limit => 8, :default => 0
+    t.string  "name"
+  end
+
+  add_index "tagged_users", ["checkin_id"], :name => "idx_checkin_id"
+  add_index "tagged_users", ["facebook_id"], :name => "idx_facebook_id"
+
   create_table "users", :force => true do |t|
-    t.integer  "facebook_id",    :limit => 8, :default => 0
+    t.integer  "facebook_id",           :limit => 8,                               :default => 0
     t.string   "third_party_id"
     t.string   "access_token"
     t.string   "full_name"
@@ -76,22 +110,45 @@ ActiveRecord::Schema.define(:version => 20110202082319) do
     t.string   "last_name"
     t.string   "gender"
     t.string   "locale"
-    t.boolean  "verified" # facebook verification boolean flag
-    t.decimal  "fetch_progress", :precision => 3, :scale => 2 # this is a [0 - 100] int value that tells the client fetch progress percent
-    t.datetime "last_fetched_checkins" # store datetime of last time checkins were fetched from facebook
-    t.datetime "last_fetched_friends" # store datetime of last time checkins were fetched from facebook
+    t.boolean  "verified"
+    t.decimal  "fetch_progress",                     :precision => 3, :scale => 2
+    t.datetime "last_fetched_checkins"
+    t.datetime "last_fetched_friends"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
-  
-  create_table "friends", :force => true do |t|
-    t.integer  "facebook_id",    :limit => 8, :default => 0
-    t.integer  "friend_id",    :limit => 8, :default => 0
-    t.integer  "degree", :limit => 1, :default => 0
+
+  add_index "users", ["facebook_id"], :name => "idx_facebook_id", :unique => true
+
+  create_table "yelp", :force => true do |t|
+    t.string   "yelp_id"
+    t.integer  "place_id",     :limit => 8,                                 :default => 0
+    t.string   "name"
+    t.string   "phone"
+    t.integer  "review_count"
+    t.decimal  "lat",                       :precision => 20, :scale => 16
+    t.decimal  "lng",                       :precision => 20, :scale => 16
+    t.string   "raw_hash"
+    t.datetime "expires_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "yelp_reviews", :force => true do |t|
+    t.string   "yelp_review_pid"
+    t.integer  "yelp_id",         :limit => 8, :default => 0
+    t.string   "excerpt"
+    t.integer  "rating",                       :default => 0
+    t.datetime "time_created"
+    t.string   "user_name"
+    t.string   "user_id"
+    t.string   "raw_hash"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "yelps", :force => true do |t|
-    t.string  "yelp_pid"
+    t.string   "yelp_pid"
     t.integer  "place_id",     :limit => 8,                                 :default => 0
     t.string   "name"
     t.string   "phone"
@@ -101,36 +158,10 @@ ActiveRecord::Schema.define(:version => 20110202082319) do
     t.string   "state"
     t.string   "country"
     t.string   "zip"
-    t.decimal  "lat",                  :precision => 20, :scale => 16
-    t.decimal  "lng",                  :precision => 20, :scale => 16
-    t.string   "raw_hash" # this stores the parsed json, raw hash from yelp, in case we need to parse stuff out of it later
-    t.datetime "expires_at" # this tells the scraper when it should rescrape the place
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-  
-  create_table "yelp_reviews", :force => true do |t|
-    t.string "yelp_review_pid" 
-    t.integer "yelp_id",      :limit  => 8, :default => 0
-    t.string  "excerpt"
-    t.integer "rating",       :limit  => 1, :default => 0
-    t.datetime  "time_created"
-    t.string  "user_name"
-    t.string  "user_id"
-    t.string  "raw_hash"
-    t.datetime "created_at"
-    t.datetime "updated_at"  
-  end
-
-  create_table "delayed_jobs", :force => true do |t|
-    t.integer  "priority", :default => 0      # jobs can jump to the front of
-    t.integer  "attempts", :default => 0      # retries, but still fail eventually
-    t.text     "handler"                      # YAML object dump
-    t.text     "last_error"                   # last failure
-    t.datetime "run_at"                       # schedule for later
-    t.datetime "locked_at"                    # set when client working this job
-    t.datetime "failed_at"                    # set when all retries have failed
-    t.text     "locked_by"                    # who is working on this object
+    t.decimal  "lat",                       :precision => 20, :scale => 16
+    t.decimal  "lng",                       :precision => 20, :scale => 16
+    t.string   "raw_hash"
+    t.datetime "expires_at"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
