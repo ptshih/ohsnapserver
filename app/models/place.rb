f1dcad06ad54a1edd17bfe9d86a4affbc5b513aa
gene tsai <sircambridge@gmail.com>
2011-02-28 00:01:45 -0800

    adding yelp scraper for Place

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,23 @@
+require 'pp'
+require 'yelpscale'
 class Place < ActiveRecord::Base
   has_one :yelp, :foreign_key => 'yelp_id', :primary_key => 'yelp_id', :inverse_of => :place
   has_one :gowalla, :foreign_key => 'gowalla_id', :primary_key => 'gowalla_id', :inverse_of => :place
+  
+  def scrapeYelp
+    result = YelpScape.new.yelpResults({'lat'=>self['lat'],'long'=>self['lng'],'query'=>self['name']})
+    pp result
+    myyelp = self.create_yelp({
+        :name => result['name'],
+        :lat =>result['lat'],
+        :lng => result['long'],
+        :review_count => result['reviews'].size
+    }).save
+    
+    # result['reviews'].each do |review|
+    #     myyelp.reviews.create({
+    #         
+    #     })
+    # end
+  end
 end
