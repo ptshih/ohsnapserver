e89c21b8f6d503aad949677494879139be9f70f1
Peter Shih <ptshih@gmail.com>
2011-03-31 04:26:00 -0700

    session and register APIs have slightly different logic. added threading for ALL CHECKINS scrape call for first batch. subsequent batches still use delayed_job. also on session calls we always hit the recent_checkins API no matter time diff

--- app/controllers/moogle_controller.rb
+++ app/controllers/moogle_controller.rb
@@ -61,18 +61,28 @@ class MoogleController < ApplicationController
     
   end
   
+  def get_friends_checkins_thread(friend_id_array = nil, last_fetched_checkins = nil)
+    # This version just fires off a thread and immediately does the scrape
+    t = Thread.new do
+      @facebook_api.find_checkins_for_facebook_id_array(@current_user.facebook_id, friend_id_array, last_fetched_checkins)
+    end
+  end
+  
   def get_friends_checkins(friend_id_array = nil, last_fetched_checkins = nil)
     # We need to split up the fb_friend_id_array here so that we don't hit the FB API throttle
-    # 600 calls per 600 seconds
+    # 600 calls per 600 seconds (maybe get unthrottled in the future)
     
     # first we get the initial slice of IDs
-    first_batch = friend_id_array.slice!(0..499)
+    first_batch = friend_id_array.slice!(0..299)
     
     sliced_first_batch = first_batch.each_slice(100).to_a
     
     sliced_first_batch.each do |first_slice|
-      first_slice_checkins = QueuedCheckins.new(@current_user.access_token, @current_user.facebook_id, first_slice, last_fetched_checkins)
-      first_slice_checkins.delay.get_friends_checkins_async
+      t = Thread.new do
+        @facebook_api.find_checkins_for_facebook_id_array(@current_user.facebook_id, friend_id_array, last_fetched_checkins)
+      end
+      # first_slice_checkins = QueuedCheckins.new(@current_user.access_token, @current_user.facebook_id, first_slice, last_fetched_checkins)
+      # first_slice_checkins.delay.get_friends_checkins_async
     end
     
     # now we slice up the remaining IDs into chunks of 500
@@ -110,11 +120,10 @@ class MoogleController < ApplicationController
     @facebook_api.find_checkins_for_facebook_id(@current_user.facebook_id, last_fetched_checkins)
     
     # We want to send the entire friendslist hash of id, name to the client
+    # Get all checkins for friends of the current user
     friend_array = Friend.find(:all, :select=>"friends.friend_id, users.full_name", :conditions=>"friends.facebook_id = #{@current_user.facebook_id}", :joins=>"left join users on friends.friend_id = users.facebook_id").map {|f| {:friend_id=>f.friend_id.to_i, :friend_name=>f.full_name}}
     friend_id_array = friend_array.map  do |f| f[:friend_id] end
-      
-    # Get all checkins for friends of the current user
-    get_friends_checkins(friend_id_array, last_fetched_checkins)
+    self.get_friends_checkins(friend_id_array, last_fetched_checkins)
     
     # The response should include the current user ID and name for the client to cache
     session_response_hash = {
@@ -139,11 +148,14 @@ class MoogleController < ApplicationController
     # if last fetched date is under 10 minutes (that is facebook's throttle), don't refetch
     if not @current_user.last_fetched_checkins.nil?
       time_diff = Time.now - @current_user.last_fetched_checkins
-    
-      puts "\n\nTime diff #{time_diff.to_i}\n\n"
+      puts "\n\nLast Session time diff #{time_diff.to_i}\n\n"
     else
       time_diff = 601
     end
+    
+    # Get all recent checkins for current user and his/her friends
+    # This API is hit to provide a fast set of data for the user to start using the app
+    @facebook_api.find_recent_checkins_for_facebook_id(@current_user.facebook_id)
     
     if time_diff.to_i > 600 then
       puts "\n\nREFETCHING\n\n"
@@ -157,19 +169,13 @@ class MoogleController < ApplicationController
       # Get all friends from facebook for the current user again with since timestamp
       @facebook_api.find_friends_for_facebook_id(@current_user.facebook_id, last_fetched_friends)
       
-      # Get all recent checkins for current user and his/her friends
-      # This API is hit to provide a fast set of data for the user to start using the app
-      @facebook_api.find_recent_checkins_for_facebook_id(@current_user.facebook_id)
-      
       # Get all checkins for current user
       @facebook_api.find_checkins_for_facebook_id(@current_user.facebook_id, last_fetched_checkins)
       
+      # # Get all checkins for friends of the current user
       friend_array = Friend.find(:all, :select=>"friends.friend_id, users.full_name", :conditions=>"friends.facebook_id = #{@current_user.facebook_id}", :joins=>"left join users on friends.friend_id = users.facebook_id").map {|f| {:friend_id=>f.friend_id.to_i, :friend_name=>f.full_name}}
       friend_id_array = friend_array.map  do |f| f[:friend_id] end
-        
-      # Get all checkins for friends of the current user
-      get_friends_checkins(friend_id_array, last_fetched_checkins)
-      
+      self.get_friends_checkins(friend_id_array, last_fetched_checkins)
     end
     
     # The response should include the current user ID and name for the client to cache
