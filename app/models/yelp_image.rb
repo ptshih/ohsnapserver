7e3d5a49133a0190848bd82d31fb4ac947d5a149
Peter Shih <ptshih@gmail.com>
2011-04-14 03:21:02 -0700

    preparing for kupo v5

--- app/models/yelp_image.rb
+++
@@ -1,3 +0,0 @@
-class YelpImage < ActiveRecord::Base
-  belongs_to :yelp, :foreign_key => 'yelp_pid', :primary_key => 'yelp_pid', :inverse_of => :yelp_reviews
-end
--- test/unit/yelp_category_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class YelpCategoryTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
-  end
-end
