b4843c6d87ac582f3115d1af101e4fd804aeca17
Peter Shih <ptshih@gmail.com>
2011-04-18 03:33:58 -0700

    auth error fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,14 @@ class ApplicationController < ActionController::Base
   
   def load_version(valid_versions = ["v1","v2","v3"])
     @version =  params[:version]
-    render_status("Error:  Invalid Version") and return false unless valid_versions.include?(@version)
+    if !valid_versions.include?(@version)
+      error_response = {}
+      error_response["error_type"] = "APIException"
+      error_response["error_message"] = "Unknown API Version"
+      render :json => error_response, :status => :unauthorized
+    end
+    
+    # render_status("Error:  Invalid Version") and return false unless valid_versions.include?(@version)
   end
   
   # Reads the fb access_token param from requests and stores the current user object
@@ -13,8 +20,14 @@ class ApplicationController < ActionController::Base
       @current_user = User.find_by_access_token(params[:access_token])
       
       # check @current_user is not nil
-      
-      load_facebook_api(@current_user.facebook_access_token)
+      if @current_user.nil?
+        error_response = {}
+        error_response["error_type"] = "AuthException"
+        error_response["error_message"] = "Unauthorized Token"
+        render :json => error_response, :status => :unauthorized
+      else
+        load_facebook_api(@current_user.facebook_access_token)
+      end
     else
       error_response = {}
       error_response["error_type"] = "AuthException"
