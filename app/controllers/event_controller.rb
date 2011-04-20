2beeff8768c2ee84faccbbffba7fc4e4cf6fc114
Tom Liou <tliou@raybeam.com>
2011-04-20 00:31:27 -0700

    Split event/kupo call into using AR and not-AR

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -5,8 +5,76 @@ class EventController < ApplicationController
     # controller.authenticate_token # sets the @current_user var based on passed in access_token (FB)
   end
   
-  # Show all kupos related to an event
+  # Show all kupos related to an event without using AR
   def kupos
+    # logging(request, actiontype, lat=nil, lng=nil, var1=nil, var2=nil)
+    Rails.logger.info request.query_parameters.inspect
+    
+    api_call_start = Time.now.to_f
+    
+    # We should limit results to 50 if no count is specified
+     limit_count = "limit 50"
+     if !params[:count].nil?
+       limit_count = "limit "+params[:count].to_s
+     end
+     
+    # Event filter
+    event_condition = "event_id = #{params[:event_id]}"
+     
+    # Content filter = Video, photo, or video and photo only
+    content_type_conditions = ""
+    if params[:type].nil?
+    elsif params[:type]=="video_only"
+      content_type_conditions = " AND has_video=1"
+    elsif params[:type]=="photo_only"
+      # it's a photo only, not a photo snapshot of video
+      content_type_conditions = " AND has_photo=1 AND has_video=0"
+    else
+      # video OR photo
+      content_type_conditions = " AND has_photo+has_video>0"
+    end
+    
+    query = "select k.*, u.facebook_id, u.name
+              from kupos k
+              join users u on k.user_id = u.id
+            where " + set_conditions + content_type_conditions + "
+            order by k.created_at desc
+            " + limit_count
+    response_array = []
+    kupos = ActiveRecord::Base.connection.execute(query)
+    kupos.each do |k|
+      row_hash = {
+        :id => k['id'],
+        :event_id => k['event_id'],
+        :author_id => k['user_id'],
+        :author_facebook_id => k['facebook_id'],
+        :author_name => k['name'],
+        :message => k['message'],
+        :has_photo => k['has_photo'],
+        :has_video => k['has_video'],
+        :photo_file_name => k['photo_file_name'],
+        :video_file_name => k['video_file_name'],
+        :timestamp => k['updated_at'].to_i
+      }
+      response_array << row_hash
+    end
+    
+    # Construct Response
+    @response_hash = {}
+    @response_hash[:data] = response_array
+    
+    api_call_duration = Time.now.to_f - api_call_start
+    LOGGING::Logging.logfunction(request,nil,'event#kupos',nil,nil,api_call_duration,params[:event_id],nil,nil)
+    
+    respond_to do |format|
+      format.html
+      format.xml  { render :xml => @response_hash }
+      format.json  { render :json => @response_hash }
+    end
+  end
+  
+  # Show all kupos related to an event
+  def kupos_ar
     # logging(request, actiontype, lat=nil, lng=nil, var1=nil, var2=nil)
     Rails.logger.info request.query_parameters.inspect
     
@@ -17,8 +85,24 @@ class EventController < ApplicationController
      if !params[:count].nil?
        limit_count = params[:count].to_i
      end
+     
+    # Video, photo, or video and photo only
+    set_conditions = "event_id = #{params[:event_id]}"
+    if params[:type].nil?
+    elsif params[:type]=="video_only"
+      set_conditions = "event_id = #{params[:event_id]} AND has_video=1"
+    elsif params[:type]=="photo_only"
+      # it's a photo only, not a photo snapshot of video
+      set_conditions = "event_id = #{params[:event_id]} AND has_photo=1 AND has_video=0"
+    else
+      # video OR photo
+      set_conditions = "event_id = #{params[:event_id]} AND has_photo+has_video>0"
+    end
     
-    kupos = Kupo.find(:all, :conditions => "event_id = #{params[:event_id]}", :order => 'created_at DESC', :limit => limit_count)
+    response_array = []
+    mysqlresults = ActiveRecord::Base.connection.execute(query)
+    
+    kupos = Kupo.find(:all, :conditions => set_conditions, :order => 'created_at DESC', :limit => limit_count)
     
     response_array = []
     kupos.each do |k|
